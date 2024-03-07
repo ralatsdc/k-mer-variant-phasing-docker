4c6207786bca05d53bcf520e3fdf3e9e7aad6fe2
Raymond LeClair <raymond.leclair@springbok.io>
2024-03-07 15:42:47 -0500

    Improve	update documentation, and ignore update	script debris

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Run this script in base image:
-# $ docker run -itv $PWD:/root continuumio/miniconda3@sha256:166ff37fba6c25fcad8516aa5481a2a8dfde11370f81b245c1e2e8002e68bcce
+# $ docker run -itv $PWD:/root -w /root continuumio/miniconda3@sha256:166ff37fba6c25fcad8516aa5481a2a8dfde11370f81b245c1e2e8002e68bcce
 eval "$(conda shell.bash hook)"
 conda create -n k-mer-variant-phasing -c conda-forge -c bioconda mamba 'snakemake>=8.4' -y
 conda activate k-mer-variant-phasing
